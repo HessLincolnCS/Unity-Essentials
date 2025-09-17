69ccc5e2eed473e3607faa4e197652a87b842c74
Lincoln <hesslincolncs1@gmail.com>
2025-09-17 08:26:04 -0400

    9/17/25

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     public float rotationSpeed;
-
+    public GameObject onCollectEffect;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +15,14 @@ public class Collectible : MonoBehaviour
     {
         
         transform.Rotate(0, rotationSpeed, 0);
-
+        
     }
+
+    private void OnTriggerEnter(Collider other) {
+    // Instantiate the particle effect
+    Instantiate(onCollectEffect, transform.position, transform.rotation);
+    // Destroy the collectible
+    Destroy(gameObject);
+    }
+
 }
