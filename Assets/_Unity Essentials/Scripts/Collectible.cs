2774ff8048fd8e3ab916104125967f699dcf932e
LincolnHo <hesslincolncs1@gmail.com>
2025-09-17 23:11:41 -0400

    Collect The Collectible

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -19,10 +19,18 @@ public class Collectible : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other) {
-    // Instantiate the particle effect
+        if (other.CompareTag("Player")) {
+              //Instantiate the particle effect
     Instantiate(onCollectEffect, transform.position, transform.rotation);
+
+
     // Destroy the collectible
     Destroy(gameObject);
-    }
+    
+        }
+       
 
+  
+    }
+    
 }
